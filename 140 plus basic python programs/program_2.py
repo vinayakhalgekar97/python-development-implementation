80a1b062110ec89f9623b626b8370d7641f56fc4
vinayakhalgekar97 <vh.developer200817@gmail.com>
2025-10-12 12:08:58 +0530

    Solving programs

--- 140 plus basic python programs/program_2.py
+++ 140 plus basic python programs/program_2.py
@@ -1,1 +1,30 @@
 # Write a Python program to do arithmetical operations addition and division.
+error = False
+
+def add(num1, num2):
+    return num1 + num2
+
+def div(num1, num2):
+    return num1 / num2
+
+try:
+    operation = input("Select an operation - '+' for addition or '/' for division: ")
+    if operation == "+":
+        num1 = input("Enter first number: ")
+        num2 = int(input("Enter second number: "))
+        result = add(num1=num1, num2=num2)
+    elif operation == "/":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        result = round(div(num1=num1, num2=num2), 2)
+    else:
+        error = True
+except (ZeroDivisionError, TypeError) as e:
+    print(f"ERROR: {e}")
+else:
+    if not error:
+        print(f"{num1} {operation} {num2} = {result}")
+    else:
+        print("Please select valid operation")
+finally:
+    print("ARITHMETIC OPERATIONS COMPLETED...")
--- 140 plus basic python programs/program_3.py
+++ 140 plus basic python programs/program_3.py
@@ -1,1 +1,15 @@
 # Write a Python program to find the area of a triangle
+def triangle_area(base, height):
+    return 0.5 * base * height
+
+try:
+    base = int(input("Enter base unit of the triangle: "))
+    height = int(input("Enter height unit of the triangle: "))
+    
+    result = triangle_area(base=base, height=height)
+except TypeError as e:
+    print(f"ERROR: {e}")
+else:
+    print(f"The area of the triangle is {result}")
+finally:
+    print("PROGRAM ENDED...")
